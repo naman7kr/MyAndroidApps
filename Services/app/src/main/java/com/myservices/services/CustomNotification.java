cec635e13cc365dd24df882bc89171ae56402d13
naman7kr <naman7kr@gmail.com>
2018-09-23 23:29:26 +0530

    temp changed to customNotification

---
+++ Services/app/src/main/java/com/myservices/services/CustomNotification.java
@@ -0,0 +1,27 @@
+package com.myservices.services;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.RemoteViews;
+
+public class CustomNotification extends AppCompatActivity {
+    private RemoteViews remoteViews;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+        findViewById(R.id.stop_temp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), NotificationService.class);
+                stopService(intent);
+            }
+        });
+    }
+
+}
